d38e126265734a39d3ed020091d8aa9338122c83
ignatandrei <ignatandrei@yahoo.com>
2016-01-05 10:56:51 +0200

    corrected export

--- Exporter/ExporterTests/TestFactory.cs
+++ Exporter/ExporterTests/TestFactory.cs
@@ -21,17 +21,21 @@ namespace ExporterTests
     { 'Name':'Andrei Ignat', 
         'WebSite':'http://msprogrammer.serviciipeweb.ro/',
         'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
+    },
+{ 'Name':'Andrei Ignat', 
+        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
+        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
     }
 ]";
                 
 
                 var p = new Person { Name = "Andrei Ignat", WebSite = "http://msprogrammer.serviciipeweb.ro/", CV = "http://serviciipeweb.ro/iafblog/content/binary/cv.doc" };
-                var byte1 = new ExportExcel2007<Person>().ExportResult(new List<Person>(){p});
+                var byte1 = new ExportExcel2007<Person>().ExportResult(new List<Person>(){p,p});
 
                 var byte2 = ExportFactory.ExportDataJson(data, ExportToFormat.Excel2007);
 
-                File.WriteAllBytes("byte1.xlsx",byte1);
-                File.WriteAllBytes("byte2.xlsx", byte2);
+                //File.WriteAllBytes("byte1.xlsx",byte1);
+                //File.WriteAllBytes("byte2.xlsx", byte2);
                 Assert.IsTrue(Math.Abs(byte1.Length-byte2.Length)<100);
 
 
