247292728222e955eda5e364d1ffbda5ff4441bb
ignatandrei <ignatandrei@yahoo.com>
2016-01-05 10:48:23 +0200

    added json array export

--- Exporter/ExportImplementation/ExportFactory.cs
+++ Exporter/ExportImplementation/ExportFactory.cs
@@ -1,22 +1,68 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using ExporterObjects;
+using Microsoft.CSharp;
+using Newtonsoft.Json.Linq;
+using RazorEngine;
+using RazorEngine.Templating;
 
 namespace ExportImplementation
 {
     public static class ExportFactory
     {
-        public static byte[] ExportData<T>(List<T> data, ExportToFormat exportFormat, params KeyValuePair<string, object>[] additionalData)
-            where T:class
+        public static byte[] ExportDataWithType(IEnumerable data, ExportToFormat exportFormat,Type type,
+            params KeyValuePair<string, object>[] additionalData)
+            
+        {
+            var exportType = typeof(Export<>).MakeGenericType(type);
+
+            switch (exportFormat)
+            {
+                case ExportToFormat.Word2003XML:
+                    
+                    exportType = typeof(ExportWord2003<>).MakeGenericType(type);
+                    break;
+                case ExportToFormat.Excel2003XML:
+                    exportType = typeof(ExportExcel2003<>).MakeGenericType(type);                    
+                    break;
+                case ExportToFormat.HTML:
+                    exportType = typeof(ExportHtml<>).MakeGenericType(type);
+                    break;
+                case ExportToFormat.PDFiTextSharp4:
+                    exportType = typeof(ExportPdfiTextSharp4<>).MakeGenericType(type);
+                    break;
+                case ExportToFormat.Word2007:
+                    exportType = typeof(ExportWord2007<>).MakeGenericType(type);
+                    break;
+                case ExportToFormat.Excel2007:
+                    exportType = typeof(ExportExcel2007<>).MakeGenericType(type);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(exportFormat), exportFormat, null);
+            }
+
+            dynamic export = Activator.CreateInstance(exportType);
+            dynamic data1 = data;
+            return export.ExportResult(data1, additionalData);
+
+        }
+
+        public static byte[] ExportData<T>(List<T> data, ExportToFormat exportFormat,
+            params KeyValuePair<string, object>[] additionalData)
+            where T : class
         {
             Export<T> export;
             switch (exportFormat)
             {
                 case ExportToFormat.Word2003XML:
-                    export=new ExportWord2003<T>();
+                    export = new ExportWord2003<T>();
                     break;
                 case ExportToFormat.Excel2003XML:
                     export = new ExportExcel2003<T>();
@@ -39,5 +85,92 @@ namespace ExportImplementation
             return export.ExportResult(data, additionalData);
         }
 
+        public class ModelRuntimeJson
+        {
+            public string ClassName { get; set; }
+            public string[] Properties { get; set; }
+        }
+
+        public static byte[] ExportDataJson(string jsonArray, ExportToFormat exportFormat,
+            params KeyValuePair<string, object>[] additionalData)
+        {
+            var jObj = JArray.Parse(jsonArray);
+            var props = jObj[0].Select(it => ((JProperty) it).Name).ToArray();
+            var constructor = string.Join(",string ", props);
+            var hash = constructor.GetHashCode();
+
+            var mrj = new ModelRuntimeJson();
+            mrj.ClassName = "Data" + hash;
+            mrj.Properties = props;
+
+            var template = @"
+using System;
+public class @Model.ClassName {
+//constructor
+public @Model.ClassName (
+    @foreach(var prop in Model.Properties){
+    <text>string @prop , </text>
+    }
+    //add a fake property
+string fake=null)
+{
+    @foreach(var prop in Model.Properties){
+    <text>this.@prop = @prop;</text>
+    }
+}//end constructor
+
+
+//properties
+@foreach(var prop in Model.Properties){
+    <text>public string @prop{get;set;}</text>
+    }
+
+
+ 
+}//end class               
+";
+            var code = Engine.Razor.RunCompile(template, mrj.ClassName, typeof (ModelRuntimeJson), mrj);
+            var provider = new CSharpCodeProvider();
+            var parameters = new CompilerParameters();
+            parameters.ReferencedAssemblies.Add("System.dll");
+            // True - memory generation, false - external file generation
+            parameters.GenerateInMemory = false;
+            // True - exe file generation, false - dll file generation
+            parameters.GenerateExecutable = false;
+            
+            var results = provider.CompileAssemblyFromSource(parameters, code);
+            if (results.Errors.HasErrors)
+            {
+                var sb = new StringBuilder();
+
+                foreach (CompilerError error in results.Errors)
+                {
+                    sb.AppendLine(string.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
+                }
+
+                throw new InvalidOperationException(sb.ToString());
+            }
+            var assembly = results.CompiledAssembly;
+
+            //in order to be found from Razor export
+            Assembly.LoadFile(assembly.Location);
+
+            var type = assembly.DefinedTypes.First(t => t.Name == mrj.ClassName);
+            var listType = typeof(List<>).MakeGenericType(type);
+            
+            dynamic list = Activator.CreateInstance(listType);
+            for (int i=0;i<jObj.Count;i++)
+            {
+                var item = jObj[i];
+                var propsValue = item.Select(it => ((JProperty)it).Value).Select(it=>it.ToString()).ToList();
+                propsValue.Add("fake");
+                dynamic obj = assembly.CreateInstance(mrj.ClassName, true, BindingFlags.Public | BindingFlags.Instance, null,
+                    propsValue.ToArray(), null,
+                    null);
+                list.Add(obj);
+
+            }
+            return ExportDataWithType(list as IEnumerable, exportFormat, type, additionalData);
+        }
     }
 }
---
+++ Exporter/ExporterTests/TestFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using ExportImplementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExporterTests
+{
+
+    public class TestFactory
+    {
+
+        [TestClass]
+        public class ExportDataJson
+        {
+            [TestMethod]
+            public void TestCorrectJson()
+            {
+                string data = @"[
+    { 'Name':'Andrei Ignat', 
+        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
+        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
+    }
+]";
+                
+
+                var p = new Person { Name = "Andrei Ignat", WebSite = "http://msprogrammer.serviciipeweb.ro/", CV = "http://serviciipeweb.ro/iafblog/content/binary/cv.doc" };
+                var byte1 = new ExportExcel2007<Person>().ExportResult(new List<Person>(){p});
+
+                var byte2 = ExportFactory.ExportDataJson(data, ExportToFormat.Excel2007);
+
+                File.WriteAllBytes("byte1.xlsx",byte1);
+                File.WriteAllBytes("byte2.xlsx", byte2);
+                Assert.IsTrue(Math.Abs(byte1.Length-byte2.Length)<100);
+
+
+            }
+        }
+    }
+}
