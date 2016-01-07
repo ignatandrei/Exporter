a6d0d5ac1f0224e6d1b642eca54e7cd855045487
ignatandrei <ignatandrei@yahoo.com>
2016-01-07 11:17:18 +0200

    re-make web

--- Exporter/ExporterWeb/Properties/AssemblyInfo.cs
+++ Exporter/ExporterWeb/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6154edb8-af30-4327-bdbe-b955343dbb06")]
+[assembly: Guid("38a17a35-2432-4f2c-bce9-61320ad2fdf2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
