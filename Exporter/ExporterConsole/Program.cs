bbbe155e9d164a24cd554292cc7905efc0de1044
ignatandrei <ignatandrei@yahoo.com>
2016-01-10 18:58:43 +0200

    now full example

--- Exporter/ExporterConsole/Program.cs
+++ Exporter/ExporterConsole/Program.cs
@@ -19,57 +19,57 @@ namespace ExporterConsole
             Export<Person> export = new ExportHtml<Person>();
             var data = export.ExportResult(new List<Person>() { p });            
             File.WriteAllBytes("a.html",data);
-//            Process.Start("a.html");
+            Process.Start("a.html");
 
-//            export=new ExportExcel2007<Person>();
-//            data = export.ExportResult(new List<Person>() { p });
-//            File.WriteAllBytes("a.xlsx", data);
-//            Process.Start("a.xlsx");
-
-
-//            export = new ExportWord2007<Person>();
-//            data = export.ExportResult(new List<Person>() { p });
-//            File.WriteAllBytes("a.docx", data);
-//            Process.Start("a.docx");
+            export = new ExportExcel2007<Person>();
+            data = export.ExportResult(new List<Person>() { p });
+            File.WriteAllBytes("a.xlsx", data);
+            Process.Start("a.xlsx");
 
 
-//            export = new ExportPdfiTextSharp4<Person>();
-//            data = export.ExportResult(new List<Person>() { p });
-//            File.WriteAllBytes("a.pdf", data);
-//            Process.Start("a.pdf");
+            export = new ExportWord2007<Person>();
+            data = export.ExportResult(new List<Person>() { p });
+            File.WriteAllBytes("a.docx", data);
+            Process.Start("a.docx");
 
 
-//            //or you can use the factory
-//            data = ExportFactory.ExportData(new List<Person>() {p}, ExportToFormat.Excel2007);
-//            File.WriteAllBytes("b.xlsx", data);
-//            Process.Start("b.xlsx");
-
-//            //export from JSON
-//            string dataJson = @"[
-//    { 'Name':'Andrei Ignat', 
-//        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
-//        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
-//    },
-//{ 'Name':'Andrei Ignat', 
-//        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
-//        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
-//    }
-//]";
-
-//            data= ExportFactory.ExportDataJson(dataJson, ExportToFormat.Excel2007);
-//            File.WriteAllBytes("bJson.xlsx", data);
-//            Process.Start("bJson.xlsx");
+            export = new ExportPdfiTextSharp4<Person>();
+            data = export.ExportResult(new List<Person>() { p });
+            File.WriteAllBytes("a.pdf", data);
+            Process.Start("a.pdf");
 
 
-//            //or from CSV
-//            var dataCSV = new List<string>();
-//            dataCSV.Add("Name,WebSite,CV");
-//            dataCSV.Add("Andrei Ignat,http://msprogrammer.serviciipeweb.ro/,http://serviciipeweb.ro/iafblog/content/binary/cv.doc");
-//            dataCSV.Add("Andrei Ignat,http://msprogrammer.serviciipeweb.ro/,http://serviciipeweb.ro/iafblog/content/binary/cv.doc");
+            //or you can use the factory
+            data = ExportFactory.ExportData(new List<Person>() { p }, ExportToFormat.Excel2007);
+            File.WriteAllBytes("b.xlsx", data);
+            Process.Start("b.xlsx");
 
-//            data= ExportFactory.ExportDataCsv(dataCSV.ToArray(), ExportToFormat.Excel2007);
-//            File.WriteAllBytes("bCSV.xlsx", data);
-//            Process.Start("bCSV.xlsx");
+            //export from JSON
+            string dataJson = @"[
+    { 'Name':'Andrei Ignat', 
+        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
+        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
+    },
+{ 'Name':'Andrei Ignat', 
+        'WebSite':'http://msprogrammer.serviciipeweb.ro/',
+        'CV':'http://serviciipeweb.ro/iafblog/content/binary/cv.doc'        
+    }
+]";
+
+            data = ExportFactory.ExportDataJson(dataJson, ExportToFormat.Excel2007);
+            File.WriteAllBytes("bJson.xlsx", data);
+            Process.Start("bJson.xlsx");
+
+
+            //or from CSV
+            var dataCSV = new List<string>();
+            dataCSV.Add("Name,WebSite,CV");
+            dataCSV.Add("Andrei Ignat,http://msprogrammer.serviciipeweb.ro/,http://serviciipeweb.ro/iafblog/content/binary/cv.doc");
+            dataCSV.Add("Andrei Ignat,http://msprogrammer.serviciipeweb.ro/,http://serviciipeweb.ro/iafblog/content/binary/cv.doc");
+
+            data = ExportFactory.ExportDataCsv(dataCSV.ToArray(), ExportToFormat.Excel2007);
+            File.WriteAllBytes("bCSV.xlsx", data);
+            Process.Start("bCSV.xlsx");
 
             //advanced - modifying templates - in this case , added Number
             export = new ExportExcel2007<Person>();
