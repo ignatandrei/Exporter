df1ead30ec35c0fae7da2fe1d370b31cdd89e557
ignatandrei <ignatandrei@yahoo.com>
2016-01-27 23:37:31 +0200

    added powershell

---
+++ powershell/Exporter.ps1
@@ -0,0 +1,15 @@
+﻿$rootFolder=$MyInvocation.MyCommand.Path
+$rootFolder=Split-Path $rootFolder
+
+$dllPath =Join-Path $rootFolder "libs" 
+
+$dllExport =Join-Path  $dllPath "ExportImplementation.dll"
+#Add-Type -LiteralPath "D:\github\Exporter\ExporterWordExcelPDF\lib\ExportImplementation.dll"
+Add-Type -LiteralPath $dllExport
+
+[string[]] $myArray=@("Name,WebSite,CV", "Andrei Ignat,http://msprogrammer.serviciipeweb.ro/,http://serviciipeweb.ro/iafblog/content/binary/cv.doc","you, http://yourwebsite.com/,cv.doc")
+$b=[ExportImplementation.ExportFactory]::ExportDataCsv( $myArray , [ExportImplementation.ExportToFormat]::Excel2007)
+$dest=Join-Path $rootFolder "a.xlsx"
+[System.IO.File]::WriteAllBytes($dest,$b)
+Start-Process -FilePath $dest
+
